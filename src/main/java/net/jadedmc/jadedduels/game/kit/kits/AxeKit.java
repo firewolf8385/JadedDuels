3ef19cacd487f83ab199fb5adb738d6d841cbe90
firewolf8385 <colbyjkrawczyk@gmail.com>
2023-10-26 12:58:25 -0400

    Adds the Axe kit

--- src/main/java/net/jadedmc/jadedduels/game/kit/kits/AxeKit.java
+++ src/main/java/net/jadedmc/jadedduels/game/kit/kits/AxeKit.java
@@ -22,50 +22,48 @@
  *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  *  SOFTWARE.
  */
-package net.jadedmc.jadedduels.game.kit;
+package net.jadedmc.jadedduels.game.kit.kits;
 
 import net.jadedmc.jadedduels.JadedDuelsPlugin;
-import net.jadedmc.jadedduels.game.kit.kits.AxeKit;
-import net.jadedmc.jadedduels.game.kit.kits.BowKit;
+import net.jadedmc.jadedduels.game.kit.Kit;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
 
-import java.util.LinkedHashSet;
-import java.util.Set;
-
-/**
- * Manages all existing kits.
- */
-public class KitManager {
-    private final Set<Kit> kits = new LinkedHashSet<>();
+public class AxeKit extends Kit {
 
     /**
-     * Creates the Kit Manager.
+     * Creates the kit.
      * @param plugin Instance of the plugin.
      */
-    public KitManager(final JadedDuelsPlugin plugin) {
-        kits.add(new BowKit(plugin));
-        kits.add(new AxeKit(plugin));
+    public AxeKit(JadedDuelsPlugin plugin) {
+        super(plugin, "axe", "Axe");
+        iconMaterial(Material.DIAMOND_AXE);
+
+        ItemStack helmet = new ItemStack(Material.DIAMOND_HELMET);
+        ItemStack chestplate = new ItemStack(Material.DIAMOND_CHESTPLATE);
+        ItemStack leggings = new ItemStack(Material.DIAMOND_LEGGINGS);
+        ItemStack boots = new ItemStack(Material.DIAMOND_BOOTS);
+
+        ItemStack shield = new ItemStack(Material.SHIELD);
+
+        ItemStack axe = new ItemStack(Material.DIAMOND_AXE);
+        ItemStack sword = new ItemStack(Material.DIAMOND_SWORD);
+        ItemStack crossbow = new ItemStack(Material.CROSSBOW);
+        ItemStack bow = new ItemStack(Material.BOW);
+        ItemStack arrows = new ItemStack(Material.ARROW, 6);
+
+        addItem(39, helmet);
+        addItem(38, chestplate);
+        addItem(37, leggings);
+        addItem(36, boots);
+
+        addItem(40, shield);
+
+        addItem(0, axe);
+        addItem(1, sword);
+        addItem(2, crossbow);
+        addItem(3, bow);
+        addItem(4, arrows);
     }
 
-    /**
-     * Get a kit from its name,
-     * @param kitName Name of the kit.
-     * @return Kit from name.
-     */
-    public Kit kit(final String kitName) {
-        for(Kit kit : this.kits) {
-            if(kit.name().equalsIgnoreCase(kitName) || kit.id().equalsIgnoreCase(kitName)) {
-                return kit;
-            }
-        }
-
-        return null;
-    }
-
-    /**
-     * Get all existing kits.
-     * @return All kits.
-     */
-    public Set<Kit> kits() {
-        return kits;
-    }
 }
