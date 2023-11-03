fcd8a64893e477b93c4272676d2d3a7204f0dd07
firewolf8385 <19617807+firewolf8385@users.noreply.github.com>
2023-11-03 01:57:34 -0400

    Fix double chat messages being sent to players who die

--- src/main/java/net/jadedmc/jadedduels/listeners/ChannelMessageSendListener.java
+++ src/main/java/net/jadedmc/jadedduels/listeners/ChannelMessageSendListener.java
@@ -1,3 +1,27 @@
+/*
+ * This file is part of JadedDuels, licensed under the MIT License.
+ *
+ *  Copyright (c) JadedMC
+ *  Copyright (c) contributors
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
 package net.jadedmc.jadedduels.listeners;
 
 import net.jadedmc.jadedchat.features.channels.events.ChannelMessageSendEvent;
@@ -9,7 +33,9 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 public class ChannelMessageSendListener implements Listener {
     private final JadedDuelsPlugin plugin;
@@ -43,6 +69,11 @@ public class ChannelMessageSendListener implements Listener {
 
         List<Player> viewers = new ArrayList<>(game.players());
         viewers.addAll(game.spectators());
+
+        // Remove duplicates.
+        Set<Player> set = new LinkedHashSet<>(viewers);
+        viewers.clear();
+        viewers.addAll(set);
 
         event.setViewers(viewers);
     }
