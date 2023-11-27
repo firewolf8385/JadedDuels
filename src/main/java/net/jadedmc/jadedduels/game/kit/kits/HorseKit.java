3217e5b7c49b8f328a5c1aed986cef78a64c10ee
firewolf8385 <19617807+firewolf8385@users.noreply.github.com>
2023-11-27 00:23:59 -0500

    Overhauled the Horse kit

--- src/main/java/net/jadedmc/jadedduels/game/kit/kits/HorseKit.java
+++ src/main/java/net/jadedmc/jadedduels/game/kit/kits/HorseKit.java
@@ -27,8 +27,10 @@ package net.jadedmc.jadedduels.game.kit.kits;
 import net.jadedmc.jadedduels.JadedDuelsPlugin;
 import net.jadedmc.jadedduels.game.Game;
 import net.jadedmc.jadedduels.game.kit.Kit;
+import net.jadedmc.jadedutils.items.ItemBuilder;
 import org.bukkit.Material;
 import org.bukkit.attribute.Attribute;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Horse;
 import org.bukkit.entity.Player;
@@ -40,19 +42,61 @@ public class HorseKit extends Kit {
         super(plugin, "horse", "Horse");
         iconMaterial(Material.DIAMOND_HORSE_ARMOR);
         exitVehicle(false);
+        naturalRegeneration(false);
+        hunger(false);
 
-        addItem(0, new ItemStack(Material.IRON_SWORD));
-        addItem(3, new ItemStack(Material.BOW));
-        addItem(5, new ItemStack(Material.GOLDEN_APPLE, 2));
-        addItem(6, new ItemStack(Material.COOKED_BEEF, 64));
+        ItemStack helmet = new ItemBuilder(Material.IRON_HELMET)
+                .addEnchantment(Enchantment.PROTECTION_PROJECTILE, 1)
+                .build();
 
-        addItem(34, new ItemStack(Material.ARROW, 64));
-        addItem(35, new ItemStack(Material.ARROW, 64));
+        ItemStack chestplate = new ItemBuilder(Material.IRON_CHESTPLATE)
+                .addEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 1)
+                .build();
 
-        addItem(39, new ItemStack(Material.IRON_HELMET));
-        addItem(38, new ItemStack(Material.IRON_CHESTPLATE));
-        addItem(37, new ItemStack(Material.IRON_LEGGINGS));
-        addItem(36, new ItemStack(Material.IRON_BOOTS));
+        ItemStack leggings = new ItemBuilder(Material.IRON_LEGGINGS)
+                .addEnchantment(Enchantment.PROTECTION_PROJECTILE, 1)
+                .build();
+
+        ItemStack boots = new ItemBuilder(Material.DIAMOND_BOOTS)
+                .addEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 1)
+                .build();
+
+        ItemStack shield = new ItemStack(Material.SHIELD);
+
+        ItemStack sword = new ItemBuilder(Material.IRON_SWORD)
+                .addEnchantment(Enchantment.DAMAGE_ALL, 1)
+                .build();
+
+        ItemStack axe = new ItemBuilder(Material.IRON_AXE)
+                .addEnchantment(Enchantment.DURABILITY, 1)
+                .build();
+
+        ItemStack bow = new ItemBuilder(Material.BOW)
+                .addEnchantment(Enchantment.DURABILITY, 3)
+                .build();
+
+        ItemStack crossbow = new ItemBuilder(Material.CROSSBOW)
+                .addEnchantment(Enchantment.PIERCING, 1)
+                .build();
+
+        ItemStack goldenApples = new ItemStack(Material.GOLDEN_APPLE, 2);
+
+        ItemStack arrows = new ItemStack(Material.ARROW, 64);
+
+        addItem(0, sword);
+        addItem(1, axe);
+        addItem(2, bow);
+        addItem(3, crossbow);
+        addItem(4, goldenApples);
+        addItem(7, arrows);
+        addItem(8, arrows);
+
+        addItem(39, helmet);
+        addItem(38, chestplate);
+        addItem(37, leggings);
+        addItem(36, boots);
+
+        addItem(40, shield);
     }
 
     @Override
@@ -68,6 +112,6 @@ public class HorseKit extends Kit {
         horse.getInventory().setArmor(new ItemStack(Material.DIAMOND_HORSE_ARMOR)); // Gives the horse armor
         horse.teleport(player);
         horse.setPassenger(player);
-        horse.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).setBaseValue(0.22);
+        horse.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).setBaseValue(0.24);
     }
 }
